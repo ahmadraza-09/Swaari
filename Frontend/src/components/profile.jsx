b983291f66d1030693f3aa757ac0582de5ea5774
Ahmad Raza <ahmadraza20082003@gmail.com>
2024-05-27 00:46:03 +0530

    Created Mobile Menu Oage

--- Frontend/src/components/profile.jsx
+++ Frontend/src/components/profile.jsx
@@ -5,7 +5,95 @@ const Profile = () => {
   return (
     <>
       <div className="profile-section">
-        This is profile Page
+        <div className="user-profile-card">
+          <div className="user-profile-card-section">
+            <div className="user-profile-card-section-left">
+              <div className="left">
+                <img className='profile-img' src="Images/profile.png" alt="" />
+              </div>
+              <div className="right">
+                <h4 className="name">
+                  Ahmad Raza
+                </h4>
+                <p className="number">
+                  9297829642
+                </p>
+              </div>
+            </div>
+            <div className="user-profile-card-section-right">
+              <i class="fa-solid fa-angle-right"></i>
+            </div>
+          </div>
+          <div className="user-profile-card-bookings">
+            Bookings
+            <i class="fa-solid fa-angle-right"></i>
+          </div>
+        </div>
+        <ul className='mobile-menu-list'>
+          <li className='mobile-menu'>
+            <div className="left">
+              <i class="fa-solid fa-handshake-angle"></i>
+              Help
+            </div>
+            <div className="right">
+               <i class="fa-solid fa-angle-right"></i>
+            </div>
+          </li>
+          <li className='mobile-menu'>
+            <div className="left">
+              <i class="fa-solid fa-truck"></i>
+              Parcel - Send Items
+            </div>
+            <div className="right">
+               <i class="fa-solid fa-angle-right"></i>
+            </div>
+          </li>
+          <li className='mobile-menu'>
+            <div className="left">
+              <i class="fa-regular fa-credit-card"></i>
+              Payment
+            </div>
+            <div className="right">
+               <i class="fa-solid fa-angle-right"></i>
+            </div>
+          </li>
+          <li className='mobile-menu'>
+            <div className="left">
+              <i class="fa-solid fa-car"></i>
+              My Rides
+            </div>
+            <div className="right">
+               <i class="fa-solid fa-angle-right"></i>
+            </div>
+          </li>
+          <li className='mobile-menu'>
+            <div className="left">
+              <i class="fa-solid fa-shield"></i>
+              Safety
+            </div>
+            <div className="right">
+               <i class="fa-solid fa-angle-right"></i>
+            </div>
+          </li>
+          <li className='mobile-menu'>
+            <div className="left">
+              <i class="fa-solid fa-hand-holding-dollar"></i>
+              Refer and Earn
+            </div>
+            <div className="right">
+               <i class="fa-solid fa-angle-right"></i>
+            </div>
+          </li>
+          <li className='mobile-menu'>
+            <div className="left">
+              <i class="fa-solid fa-gear"></i>
+              Settings
+            </div>
+            <div className="right">
+               <i class="fa-solid fa-angle-right"></i>
+            </div>
+          </li>
+        </ul>
       </div>
     </>
   )
